aec163fcd06d2851e06925ddf23d4a97dbffd536
joel-plummer <joecodescs@gmail.com>
2023-12-10 16:35:49 -0500

    Pages Become Visible/Hidden on click

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", function(){
     var HomeBtn= document.getElementById('HomeBtn');
     var NewContactBtn = document.getElementById('NewContactBtn');
     var UsersBtn = document.getElementById('UsersBtn');
@@ -7,11 +7,37 @@ window.onload = function() {
     var NewContactPage= document.getElementById('new-contact');
     var UsersPage = document.getElementById('users');
     var LogoutPage= document.getElementById('logout');
-    var divs = document.querySelectorAll('div');
+    var pages = document.getElementsByClassName('page');
+    Array.from(pages).forEach(function(page){
+        page.classList.add("hidden");});
 
-    NewContactBtn.addEventListener("click", function(e){
+
+    HomeBtn.addEventListener("click", function(){
+        Array.from(pages).forEach(function(page){
+            page.classList.add("hidden");});
+    
+    });
+
+    NewContactBtn.addEventListener("click", function(){
+        Array.from(pages).forEach(function(page){
+            page.classList.add("hidden");});
         NewContactPage.classList.remove("hidden");
     
     });
 
-}
+    UsersBtn.addEventListener("click", function(){
+        Array.from(pages).forEach(function(page){
+            page.classList.add("hidden");});
+        UsersPage.classList.remove("hidden");
+    
+    });
+
+    LogoutBtn.addEventListener("click", function(){
+        Array.from(pages).forEach(function(page){
+            page.classList.add("hidden");});
+        LogoutPage.classList.remove("hidden");
+    
+    });
+
+
+});
